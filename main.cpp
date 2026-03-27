210afc39b99befd3c04ceba04e386844464625f7
sandrahanuschik <148901493+sandrahanuschik@users.noreply.github.com>
2026-03-26 21:19:35 -0400

    Created printMainMenu function

--- main.cpp
+++ main.cpp
@@ -2,9 +2,24 @@
 // Created by Sandi on 3/23/26.
 //
 #include <iostream>
+#include <vector>
+#include <string>
+#include "record.h"
+#include "load_csv.h"
 using namespace std;
 
 int main() {
-    cout << "Project starting..." << endl;
-    return 0;
+    void printMainMenu();
+}
+
+void printMainMenu() {
+    cout << "\n=========================================\n";
+    cout << " Greenhouse Gas Emissions Sorting Tool\n";
+    cout << "=========================================\n";
+    cout << "[1] Choose Sort Key\n";
+    cout << "[2] Run Merge Sort\n";
+    cout << "[3] Run Quick Sort\n";
+    cout << "[4] View Top Ten Records\n";
+    cout << "[5] Exit\n";
+    cout << "Enter Choice: ";
 }

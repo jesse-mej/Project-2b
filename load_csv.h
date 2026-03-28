cbac0558aae5565e8632f843c6aa00904d8c339d
sandrahanuschik <148901493+sandrahanuschik@users.noreply.github.com>
2026-03-27 21:50:17 -0400

    push previous changes to record struct and csv loader

--- load_csv.h
+++ load_csv.h
@@ -11,5 +11,6 @@
 using namespace std;
 
 bool loadCSV(const string& filename, vector<Record>& data);
-
+// reads a CSV file and stores each row as a record struct
+// returns true if file is successfully loaded
 #endif //PROJECT_2B_LOAD_CSV_H
--- record.h
+++ record.h
@@ -8,7 +8,7 @@
 #include <string>
 using namespace std;
 
-struct Record {
+struct Record { // represents a single row from the dataset
     string country;
     int year;
     double co2;
@@ -21,21 +21,21 @@ struct Record {
     double otherSector;
 };
 
-enum class sortingCategories {
+enum class sortingCategories { // options the user can sort by
     EMISSION_TYPE = 1,
     YEAR,
     COUNTRY,
     SECTOR
 };
 
-enum class emissionType {
+enum class emissionType { // options for emission to sort by if chosen previously
     CO2 = 1,
     N2O,
     CH4,
-    NONE
+    NONE // used for when not sorting by emission
 };
 
-enum class sectorType {
+enum class sectorType { // options for sectors to sort by if chosen previously
     POWER_INDUSTRY = 1,
     BUILDINGS,
     TRANSPORT,

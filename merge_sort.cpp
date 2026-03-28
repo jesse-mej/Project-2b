35d525d0824290ccbf3a38f09675acd8d4587e1b
sandrahanuschik <148901493+sandrahanuschik@users.noreply.github.com>
2026-03-28 13:56:28 -0400

    more printing tweaks

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -151,6 +151,9 @@ void mergeAndPrint(vector<Record>& emissionData, string category, bool descendin
         if (category == "country") {
             cout << emissionData[i].country << " - " << emissionData[i].year << endl;
         }
+        else if (category == "year") {
+            cout << emissionData[i].country << " - " << emissionData[i].year << endl;
+        }
         else {
             // print out country - year - category value for these rankings
             cout << emissionData[i].country << " - " << emissionData[i].year << " - " << categoryValue(emissionData[i], category) << endl;
